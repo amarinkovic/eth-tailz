a20aba71bb1ea755fe2a8b6325472f590728bf2c
amarinkovic <alexandar.marinkovic@gmail.com>
2023-07-14 16:05:48 +0200

    feat: stream until current block

--- src/main/scala/io/mankea/eth/streamer/App.scala
+++ src/main/scala/io/mankea/eth/streamer/App.scala
@@ -19,19 +19,19 @@ object App extends ZIOAppDefault {
 
   private val contractAddress = "0x7E5462DA297440D2a27fE27d1F291Cf67202302B"
   private val pollingInterval = 3.seconds
-  private val from = 3276471
+  private val from = 3276471 // block when it's deployed
 
   private def logStream(contractAddress: String, initialFrom: BigInt): ZStream[Web3Service, Throwable, EthLogEvent] = {
-    ZStream.unwrap {
-      ZStream.fromZIO(ZIO.service[Web3Service]).flatMap { web3 =>
-        ZStream.unfoldChunkZIO(initialFrom) { from =>
-          for {
-            currentBlock <- web3.getCurrentBlockNumber
-            to <- ZIO.succeed(currentBlock.min(from + 1000))
-            logs <- web3.getLogs(contractAddress, from, to)
-          } yield Some((Chunk.fromIterable(logs), to + 1))
-        }
-      }.flattenChunks
+    ZStream.fromZIO(ZIO.service[Web3Service]).flatMap { web3 =>
+      ZStream.unfoldChunkZIO(initialFrom) { from =>
+        for {
+          currentBlock <- web3.getCurrentBlockNumber
+          to <- ZIO.succeed(currentBlock.min(from + 1000))
+          logs <- web3.getLogs(contractAddress, from, to)
+        } yield
+          if (to == currentBlock) None
+          else Some((Chunk.fromIterable(logs), to + 1))
+      }
     }
   }
 
