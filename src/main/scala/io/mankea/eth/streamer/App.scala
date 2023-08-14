d6e453fced849db81f1bbc5ba811c98921bf8db2
amarinkovic <alexandar.marinkovic@gmail.com>
2023-08-14 22:48:07 +0200

    feat: add more error handling

--- src/main/scala/io/mankea/eth/streamer/App.scala
+++ src/main/scala/io/mankea/eth/streamer/App.scala
@@ -47,7 +47,9 @@ object App extends ZIOCliDefault {
         for {
           currentBlock <- web3Service.getCurrentBlockNumber
           to <- ZIO.succeed(currentBlock.min(from + chunkSize))
-          logs <- web3Service.getLogs(contractAddress, from, to)
+          logs <- web3Service.getLogs(contractAddress, from, to).catchAll { error =>
+            Console.printLine(s"Failed getting chunk #$from -> #$to due to: ${error.getMessage}").as(List.empty[EthLogEvent])
+          }
           _ <- if (to == currentBlock && forever && logs.isEmpty) {
             Console.printLine(waitMessage) *> ZIO.sleep(pollingInterval)
           } else ZIO.unit
